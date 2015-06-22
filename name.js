21552fec25d9220fed73871cbf12a698394c9186
Tim <tklein@outlook.com>
2015-06-22 12:03:17 -0600

    Final Commit

---
+++ name.js
@@ -0,0 +1,53 @@
+// var name = prompt('HEY YOU!!! - What is your name?');
+
+// alert('Hey ' + name);
+
+// for(var i = 0; i <= 500; i += 100){
+//     console.log(i);
+// }
+
+// for(var i = 1; i <= 100; i += i){
+//     console.log(i);
+// }
+
+// >>>>>>>> Cheat! <<<<<<<<<
+// for(var i=1; i <= 3; i++) {
+// 	console.log(i);
+// 	console.log(i);
+// 	console.log(i);
+// }
+
+// >>>>>>>> Best <<<<<<<<
+// for(var i = 1; i <= 3; i++) {
+// 	for (var count = 1; count<=3; count++) {
+// 		console.log(i);
+// 	}
+// }
+
+// >>>>>>>> Logic Helper <<<<<<<<
+// for(var i = 1; i <= 3; i++) {
+// 	for (var count = 1; count<=3; count++) {
+// 		for (var count2 = 1; count2<=3; count2++) {
+// 			console.log(i, count, count2);
+// 		}
+// 	}
+// }
+
+// for (var i = 0; i <=10; i += 2) {
+// 	console.log(i);
+// }
+
+// for (var i = 3; i <= 15; i += 3) {
+// 	console.log(i);
+// }
+
+// for (var i=9; i>=0; i--) {
+// 	console.log(i);
+// }
+
+// Inverted for loop for restarting counts
+for (var i = 1; i <= 3; i++) {
+	for (var count = 0; count<=3; count++) {
+		console.log(i, count);
+	}
+}
